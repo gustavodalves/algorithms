f8c27cc7379d73d06b90c9e0ff88b72c30e829d4
Gustavo Duarte ALves <gualves2001@icloud.com>
2024-01-01 20:59:35 -0300

    test(Fibonacci): add fibonacci input equal 1

--- fibonacci/fibonacci.spec.ts
+++ fibonacci/fibonacci.spec.ts
@@ -7,9 +7,21 @@ describe("fibonacci", () => {
         expect(value).toBe(55)
     })
 
+    it("should be able calculate fibonacci input value equal 1", () => {
+        const value = fibonacci(1)
+
+        expect(value).toBe(1)
+    })
+
     it("should be able calculate fibonacci", () => {
         const value = fibonacciDynamic(10)
 
         expect(value).toBe(55)
+    })
+
+    it("should be able calculate fibonacci dynamic input value equal 1", () => {
+        const value = fibonacciDynamic(1)
+
+        expect(value).toBe(1)
     })
 })
